b94cd0bc3a7ba2075052362eac2b303e0204d726
Isidore-M <isidore.mbargamanga@triosstudent.com>
2025-09-18 01:40:40 -0400

    Project created and Assets uploaded

---
+++ Assignment1/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Assignment1
+//
+//  Created by Eezy Mongo on 2025-09-17.
+//
+
+import SwiftUI
+
+struct Item: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Item()
+}
